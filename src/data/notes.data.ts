82ec986f018df0597191eff3f950bca04df59a98
Spiitzii <spitzi@online.de>
2024-04-29 15:21:18 +0200

    test Notes changed

--- src/data/notes.data.ts
+++ src/data/notes.data.ts
@@ -6,21 +6,29 @@ export const notesData: Note[] = [
       title: 'Überschrift 1',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Büro', 'Haushalt'],
-      id: 1
+      id: 1,
+      name: 'Admin',
+      date: '29.04.2024'
     }, {
       title: 'Überschrift 2',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Büro'],
-      id: 2
+      id: 2,
+      name: 'Admin',
+      date: '29.04.2024'
     }, {
       title: 'Überschrift 3',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Haushalt'],
-      id: 3
+      id: 3,
+      name: 'Admin',
+      date: '29.04.2024'
     }, {
       title: 'Überschrift 4',
       content: 'Kurzer Text',
       categories: [],
-      id: 4
+      id: 4,
+      name: 'Admin',
+      date: '29.04.2024'
     }
   ]
